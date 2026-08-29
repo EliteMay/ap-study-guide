402c37c44eec092a662c5bd3d2af82dc915ca290
EliteMay <k12m45k@gmail.com>
2026-08-30 02:02:55 +0900

    Bump AP Study Notes build to r6

--- js/shell.js
+++ js/shell.js
@@ -1,7 +1,7 @@
 (() => {
   'use strict';
 
-  const BUILD = '2026.08.29-r5';
+  const BUILD = '2026.08.30-r6';
   const THEME_KEY = 'ap-study-theme';
   const RECENT_KEY = 'ap-study-recent-v1';
   const BOOKMARK_KEY = 'ap-study-bookmarks-v1';
